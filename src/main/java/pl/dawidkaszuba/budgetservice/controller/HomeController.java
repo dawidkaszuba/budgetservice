e8056abe451f1b1ab7baf7ad5debfdd38871beea
dawidkaszuba <dawidk89@gmail.com>
2023-02-12 21:40:40 +0100

    draft

---
+++ src/main/java/pl/dawidkaszuba/budgetservice/controller/HomeController.java
@@ -0,0 +1,15 @@
+package pl.dawidkaszuba.budgetservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "<h1>Hello</h1>";
+    }
+}
